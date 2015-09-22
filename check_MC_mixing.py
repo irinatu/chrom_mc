0b51e7b230392a0ef0873dcfad08860fb4aec40f
Irina Tuszynska <irina@mimuw.edu.pl>
2015-09-22 18:56:48 +0200

    Fixing errors in check_MC_mixing.py

--- check_MC_mixing.py
+++ check_MC_mixing.py
@@ -45,21 +45,24 @@ def extract_coord(file):
     
 def subset_prep(coor_list, mid, pol_l):
     intra_dist_list = []
-    inter_mat = np.zeros((plo_l, pol_l), float)
     steps = [mid *k/20 for k in range(10,21)]
     for s in steps:
+        inter_mat = np.zeros((pol_l, pol_l), float)
         for i in range(pol_l):
             for j in range(i, pol_l):
-                dis = np.sqrt(np.sum((coor_list[s][i] - coord_list[s][j])**2))
+                dis = np.sqrt(np.sum((coor_list[s][i] - coor_list[s][j])**2))
+                #print "DISTANCE", dis
                 inter_mat[i,j] = inter_mat[j,i] = dis
-        intra_dist_list.append(inter_mat)                
+        intra_dist_list.append(inter_mat)
+    return intra_dist_list                
 
 def aver_dist(sub1, sub2):
     dis_list = []
     for su in sub1:
         for us in sub2:
-            dis_mat = np.sqrt(np.sum((su-us)**2)) 
-            dis_list.append(dis_mtx)
+            dis_mat = np.sqrt(np.sum(np.power((su-us),2))) 
+            #print dis_mat
+            dis_list.append(dis_mat)
     return np.mean(dis_list)           
     
     
@@ -79,12 +82,20 @@ if len(coord_from_traj1) == len(coord_from_traj2):
 elif len(coord_from_traj1) < len(coord_from_traj2): 
     middle = len(coord_from_traj1)/2
 else: middle = len(coord_from_traj2)/2
+incr = middle/2
+whole = middle*2
 
-subset_1 = subset_prep(coord_from_traj1, middle, pol_len)
-subset_2 = subset_prep(coord_from_traj2, middle, pol_len)
-av1 = aver_dist(subset_1, subset_1)
-av12 = aver_dist(subset_1, subset_2)
-print av1, av12 
-
+#print len(coord_from_traj1), len(coord_from_traj2)
+while middle < whole:
+    subset_1 = subset_prep(coord_from_traj1, middle, pol_len)
+    subset_2 = subset_prep(coord_from_traj2, middle, pol_len)
+    print pol_len, middle
+    av1 = aver_dist(subset_1, subset_1)
+    av12 = aver_dist(subset_1, subset_2)
+    print av1, av12 
+    if av12 > av1-av1*0.1 and av12 < av1+av1*0.1:
+        print middle, "is enought steps", av1, av12
+        break
+    else: middle = middle + incr
 
  
