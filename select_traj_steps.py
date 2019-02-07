d9d7f14a7a88d21e4c02ac01e55e4bc614f021aa
irinatu <irina@mimuw.edu.pl>
2019-02-07 19:54:06 +0100

    some changes

--- select_traj_steps.py
+++ select_traj_steps.py
@@ -1,6 +1,31 @@
 ## for giving trajectory/ies chose every defined number of frames
 import numpy as np
-import os
+import os, argparse, sys
+
+def pars_inp():
+    ## read/validate command-line arguments
+    argparser = argparse.ArgumentParser( usage = "%prog  [<options>]")
+
+    argparser.add_argument('-i',
+        dest = "Inp",
+        type=str,
+        nargs='+',
+        help = "input file/s with pdb trajectories")
+    argparser.add_argument('-o', type = str,
+        dest = "Out",
+        default = '',
+        help = "A output file with pdb trajectory")
+    argparser.add_argument('-s', type = int,
+        dest = "Skipp",
+        default = 50,
+        help = "Skipping frames in trajectory/ies. The smallest number is 2 - no skipping")
+        
+    opts = argparser.parse_args()
+    #if len(args) < 2:
+    #print opts
+     #   print optparser.format_help() #prints help if no arguments
+     #   sys.exit(1)
+    return opts	
 
 def find_lines_in_model(my_file, out): #find first HEADER and calculate number of positions for one model
     find = 0
@@ -23,21 +48,24 @@ def write_model(m_file, out_f):
             if find == 2: break
         out_f.write(line)
 
-
-N = 10 #number of skipping models    
-traj = open("test_A.pdb")
-out_traj = open("output_test.pdb","w")
-file_size = os.fstat(traj.fileno()).st_size
+opts = pars_inp()
+#print opts.Skipp
+N = opts.Skipp #number of skipping models    
+traj = open(opts.Inp[0])
+#print opts.Inp
+out_traj = open(opts.Out,"w")
 lines_for_model  = find_lines_in_model(traj, out_traj)
 print lines_for_model
-traj.seek(0)
-while 1:
-    traj.seek((N-2)*lines_for_model, 1)
-    print traj.tell(), file_size
-    if (traj.tell() + (N-2)*lines_for_model) >= file_size: 
-        out_traj.close()
-        break
-    else: write_model(traj, out_traj)
-
+for plik in opts.Inp:
+    tra =open(plik)
+    file_size = os.fstat(tra.fileno()).st_size
+    while 1:
+        tra.seek((N-2)*lines_for_model, 1)
+        print tra.tell(), file_size
+        if (tra.tell() + (N-2)*lines_for_model) >= file_size: #zeby za daleko nie skoczyc
+            tra.close()
+            break
+        else: write_model(tra, out_traj)
+out_traj.close()
  
 
